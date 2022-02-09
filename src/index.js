90968619930df76df2180cee017ca79e84d82978
SolloVey <chelnokow@gmail.com>
2022-02-09 21:17:15 +0300

    Add slider and edits in styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // Сбрасываем стили по умолчанию, подключаем normalize.css
 import 'normalize.css'
 
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 // Подключение к проекту фала со стилями.
 import './styles/main.scss';
 
@@ -11,3 +16,32 @@ import './styles/main.scss';
 
 // Метод используется просто подключая непосредственно файл index.html.
 import './index.html';
+
+// Подключаем библиотеку jQuery
+import $ from 'jquery';
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+});
+
+
+
+
+
+
