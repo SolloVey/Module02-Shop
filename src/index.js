4c7af060c572fb3e87cbc7a8a4aad52738a3a09d
SolloVey <chelnokow@gmail.com>
2022-08-29 21:15:29 +0300

    Add counter script

--- src/index.js
+++ src/index.js
@@ -99,12 +99,12 @@ list.addEventListener('mouseenter', function () {
 });
 
 // Появление tooltips при наведении
-let buttonOne = document.getElementById('btn_1');
-let tooltipOne = document.getElementById('FoodOne');
-let buttonTwo = document.getElementById('btn_2');
-let tooltipTwo = document.getElementById('FoodTwo');
-let buttonThree = document.getElementById('btn_3');
-let tooltipThree = document.getElementById('FoodThree');
+let buttonOne = document.querySelector('.main__btn_1');
+let tooltipOne = document.querySelector('.main__about_1');
+let buttonTwo = document.querySelector('.main__btn_2');
+let tooltipTwo = document.querySelector('.main__about_2');
+let buttonThree = document.querySelector('.main__btn_3');
+let tooltipThree = document.querySelector('.main__about_3');
 
 buttonOne.onmouseover = function () {
     tooltipOne.style.visibility = 'visible';
@@ -112,18 +112,22 @@ buttonOne.onmouseover = function () {
 buttonOne.onmouseout = function () {
     tooltipOne.style.visibility = 'hidden';
 };
+
 buttonTwo.onmouseover = function() {
     tooltipTwo.style.visibility = 'visible';
 };
 buttonTwo.onmouseout = function () {
     tooltipTwo.style.visibility = 'hidden';
 };
+
 buttonThree.onmouseover = function () {
     tooltipThree.style.visibility = 'visible';
 };
 buttonThree.onmouseout = function () {
     tooltipThree.style.visibility = 'hidden';
 };
+
+
 
 // Скрипт для добавления товаров "добавить еще"
 // window.onload = function () {
@@ -146,3 +150,48 @@ buttonThree.onmouseout = function () {
 //         }
 //     })
 // }
+
+
+
+// COUNTER
+document.querySelectorAll('.counter').forEach(counter => {
+    let counterInput = counter.querySelector('.counter__input');
+    let btnMinus = counter.querySelector('.counter__minus');
+    let btnPlus = counter.querySelector('.counter__plus');
+
+    let __onCounterControlClick = (event) => {
+        if (event.target.classList.contains('counter__plus')) {   
+            counterInput.value++;
+        }
+        if (event.target.classList.contains('counter__minus')) {
+            if (counterInput.value <= 1) {
+                return;
+            }
+            counterInput.value--;
+        }
+        var event = new Event('change');
+        counterInput.dispatchEvent(event);
+    };
+
+    let __onCounterInputChanged = (event) => {
+        let val1 = Math.round(counterInput.value);
+
+        // console.log(typeof val1, val1);
+        if (typeof val1 != 'number' || val1 < 1 || val1 === NaN) {
+            // console.log('-');
+            val1 = 1;
+        }
+        counterInput.setAttribute('value', val1);
+
+        if (counterInput.value <= 1) {
+            btnMinus.classList.add('counter__disabled');
+        } else {
+            btnMinus.classList.remove('counter__disabled');
+        }
+        // console.log('val is now = ' + counterInput.value);
+    };
+
+    btnMinus.addEventListener('click', __onCounterControlClick);
+    btnPlus.addEventListener('click', __onCounterControlClick);
+    counterInput.addEventListener('change', __onCounterInputChanged);
+});
